ab248e750b31633b41ca334e38006275e80efed5
BY <devblossom77@gmail.com>
2020-10-15 17:01:18 +0900

    Misc...

--- src/pages/Community/Gallery.js
+++ src/pages/Community/Gallery.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styled from "styled-components";
+import PageTitle from "../../components/Titles/PageTitle";
+import Detail from "../../components/Post/Card/Detail";
+
+import {mock} from "../../mock/mock";
+import List from "../../components/Post/Card/List";
 
 const Gallery = (props) => {
 
     return (
         <Wrapper>
-           Gallery
+           <PageTitle title={'Gallery'}/>
+           {/*<List/>*/}
+
+           {/*ui test*/}
+           <Detail item={mock.gallery[0]} list={mock.gallery}/>
         </Wrapper>
     )
 };
