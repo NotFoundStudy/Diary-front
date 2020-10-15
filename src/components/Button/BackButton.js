582f2aa7c181ff2cb76c89c2962cfe6e6436a2fa
BY <devblossom77@gmail.com>
2020-10-15 15:56:14 +0900

    [cmp] 뒤로가기 버튼 추가

---
+++ src/components/Button/BackButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled, {css} from "styled-components";
+import Button from "./Button";
+import HISTORY from "../../helper/historyHelper";
+
+const BackButton = (props) => {
+    const{
+        text = '뒤로가기'
+    } = props;
+
+    return (
+        <Wrapper onClick={()=> HISTORY.goBack()}>
+            {text}
+        </Wrapper>
+    )
+};
+
+const Wrapper = styled(Button)`
+  ${props => props.style1 && css`
+  
+  `}
+`;
+
+export default BackButton;
