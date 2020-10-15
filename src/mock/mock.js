f32b155068abf48e68c8cc8cb1d78b812aa825d9
BY <devblossom77@gmail.com>
2020-10-15 14:43:49 +0900

    currentMemeber 목데이터 추가

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,4 +1,19 @@
 
 export const mock = {
-    sample: []
+    currentMember: [
+        {
+            id: 0,
+            thumbnail: 'https://composite.pusan.ac.kr/sites/composite/atchmnfl/pnuProfl/1055/temp_1527566174779100.jpg',
+            name : '김3샘플',
+            position: '박사과정 (Ph.D. Candidate) | part time',
+            field: '복합재료 제조 및 난연 특성 평가',
+        },
+        {
+            id: 1,
+            thumbnail: 'https://composite.pusan.ac.kr/sites/composite/atchmnfl/pnuProfl/1055/temp_1527566174779100.jpg',
+            name : '하종록 (JONG ROK JA)',
+            position: '박사과정 (Ph.D. Candidate) | part time',
+            field: '복합재료 제조 및 난연 특성 평가',
+        }
+    ]
 };
