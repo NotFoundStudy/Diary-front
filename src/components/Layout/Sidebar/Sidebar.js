ab5a2bb75878372e91c9b10ef9865ed2aa79780a
BY <devblossom77@gmail.com>
2020-10-16 17:33:03 +0900

    [cmp] recursiveMenu 추가 (recurive, 기본값 구현 필요)

--- src/components/Layout/Sidebar/Sidebar.js
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -1,74 +1,108 @@
 import React from 'react';
 import styled from "styled-components";
-import {Menu} from 'antd';
-import {AppstoreOutlined, MailOutlined, SettingOutlined} from '@ant-design/icons';
+import {Menu as AntdMenu} from 'antd';
+import {AppstoreOutlined} from '@ant-design/icons';
+import {navigate} from "../../../helper/historyHelper";
 
-const {SubMenu} = Menu;
+const {SubMenu} = AntdMenu;
 
 const Sidebar = (props) => {
+    const {
+        list = [ // sample
+            {
+                key: '/introduction',
+                icon: <AppstoreOutlined/>,
+                name: 'Introduction',
+                sub_menu: []
+            },
+            {
+                key: '/introduction/professor',
+                icon: <AppstoreOutlined/>,
+                name: 'Professor',
+                sub_menu: []
+            },
+            {   // key, icon, name, sub_menu 필수 항목
+                key: 'introduction/people', // url값 ( string만 입력 )
+                icon: <AppstoreOutlined/>, // 아이콘이 없다면 ''
+                name: 'People', // 1depth메뉴의 이름, 서브메뉴의 타이틀
+                sub_menu: [ // if(sub_menu)로 recursiveMenu(v) 순회 여부 결정
+                    {
+                        key: 'introduction/people/currentMember',
+                        icon: '',
+                        name: 'currentMember',
+                        // sub_menu: [
+                        //     {
+                        //         key: '6',
+                        //         icon: '',
+                        //         name: 'option66',
+                        //         sub_menu: []
+                        //     },
+                        //     {
+                        //         key: '7',
+                        //         icon: '',
+                        //         name: 'option77',
+                        //         sub_menu: []
+                        //     },
+                        // ]
+                    },
+                    {
+                        key: 'introduction/people/alumni',
+                        icon: '',
+                        name: 'alumni',
+                    },
+                ]
+            }
+        ],
+        style,
+    } = props;
 
-    // [rwd] sm 사이즈에서 왼쪽 사이드바 삭제 & 헤더 드롭다운 방식으로 변경
+    // localstorage등 에서 가져와서 하기 defaultSelcted등 세팅(여기서 하기)
 
-    const handleClick = e => {
-        console.log('click ', e);
-    };
+    function recursiveMenu(subMenu) {
+        subMenu.map(menu => <>
+                {
+                    menu.sub_menu
+                        ? recursiveMenu(menu.sub_menu)
+                        : <AntdMenu.Item key={menu.key}>{menu.name}</AntdMenu.Item>
+
+                }
+            </>
+        )
+    }
 
     return (
-        <Wrapper>
-            <Menu
-                onClick={handleClick}
-                style={{width: 256}}
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                mode="inline"
-            >
-                <SubMenu
-                    key="sub1"
-                    title={
-                        <span>
-              <MailOutlined/>
-              <span>Navigation One</span>
-            </span>
-                    }
-                >
-                    <Menu.ItemGroup key="g1" title="Item 1">
-                        <Menu.Item key="1">Option 1</Menu.Item>
-                        <Menu.Item key="2">Option 2</Menu.Item>
-                    </Menu.ItemGroup>
-                    <Menu.ItemGroup key="g2" title="Item 2">
-                        <Menu.Item key="3">Option 3</Menu.Item>
-                        <Menu.Item key="4">Option 4</Menu.Item>
-                    </Menu.ItemGroup>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<AppstoreOutlined/>} title="Navigation Two">
-                    <Menu.Item key="5">Option 5</Menu.Item>
-                    <Menu.Item key="6">Option 6</Menu.Item>
-                    <SubMenu key="sub3" title="Submenu">
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
-                </SubMenu>
-                <SubMenu
-                    key="sub4"
-                    title={
-                        <span>
-              <SettingOutlined/>
-              <span>Navigation Three</span>
-            </span>
-                    }
-                >
-                    <Menu.Item key="9">Option 9</Menu.Item>
-                    <Menu.Item key="10">Option 10</Menu.Item>
-                    <Menu.Item key="11">Option 11</Menu.Item>
-                    <Menu.Item key="12">Option 12</Menu.Item>
-                </SubMenu>
-            </Menu>
-        </Wrapper>
+        <StyledMenu
+            onClick={(e) => navigate(e.key)}
+            // defaultSelectedKeys={['1']} // unit depth menu
+            // defaultOpenKeys={['sub1']} // sub menu
+            // onSelect={(e) => console.log('@@ onSelect', e)} // 로컨스토리지에 저장 & 네브 메뉴에서 학생 눌렀을떄 열려야함
+            style={style}
+            mode="inline">
+            {
+                list.map(item =>
+                    item.sub_menu.length > 0
+                        ? <SubMenu key={item.key} icon={item.icon} title={item.name}>
+                            {/*{*/}
+                            {/*    recursiveMenu(item.sub_menu)*/}
+                            {/*}*/}
+                            {
+                                item.sub_menu.map(menu =>
+                                    <AntdMenu.Item key={menu.key}>{menu.name}</AntdMenu.Item>
+                                )
+                            }
+                        </SubMenu>
+                        : <AntdMenu.Item key={item.key} icon={item.icon}>
+                            {item.name}
+                        </AntdMenu.Item>
+                )
+            }
+        </StyledMenu>
     )
 };
 
-const Wrapper = styled.div`
-  background:#afe8ff;
+const StyledMenu = styled(AntdMenu)`
+  width: 256px;
+  ${props => props.style && {...props.style}};
 `;
 
 export default Sidebar;
