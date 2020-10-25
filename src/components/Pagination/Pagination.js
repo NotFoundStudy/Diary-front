d18ceb021ef6e3e384662bcb9fcc1863bc3f3e04
BY <devblossom77@gmail.com>
2020-10-25 22:48:46 +0900

    [cmp] 페이지네이션 ui 변경중

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from "styled-components";
 import {Pagination as AntdPagination} from 'antd';
 
 const Pagination = (props) => {
     const {
-        defaultCurrent = 1,
         total,
+        onShowSizeChange = true,
+        style,
     } = props;
 
+    useEffect(()=>{
+        // 새로고침 시 url에서 page='' 가져와서 current 넣기
+        // const url2 = new URL('https://developer.mozilla.org/en-US/docs/Web/API/URL/search?q=123&page?=12');
+        const url2 = new URL(window.location.href);
+        console.log(url2.search);
+
+   },[])
+
     return (
-        <Wrapper>
-            <AntdPagination defaultCurrent={defaultCurrent} total={total}/>
+        <Wrapper style={style}>
+            <AntdPagination defaultCurrent={1}
+                            total={total}
+                            onShowSizeChange={onShowSizeChange}
+            />
         </Wrapper>
     )
 };
 
 const Wrapper = styled.div`
+  text-align: center;
 `;
 
 export default Pagination;
--- src/components/Post/PhotoPost/List.js
+++ src/components/Post/PhotoPost/List.js
@@ -14,7 +14,10 @@ const List = (props) => {
             {
               list.map(item => <Post key={item.id} item={item}/>)
             }
-            <Pagination defaultCurrent={1} total={50} />
+            <Pagination current={1}
+                        total={50}
+                        onShowSizeChange={true}
+                        style={{marginTop: '30px'}}/>
         </Wrapper>
     )
 };
