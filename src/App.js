278867a15abcae3a451a2e5b39bd9eb2414187fc
BY <devblossom77@gmail.com>
2020-10-25 22:49:12 +0900

    [App] 피드백1차 메뉴에 따른 라우터 변경 중

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Introduction from "./pages/Introduction/Introduction/Introduction";
 import Professor from "./pages/Introduction/Professor/Professor";
-import CurrentMember from "./pages/Introduction/CurrentMember/CurrentMember";
-import Alumni from "./pages/Introduction/Alumni/Alumni";
+import CurrentMember from "./pages/Members/CurrentMember/CurrentMember";
+import Alumni from "./pages/Members/Alumni/Alumni";
 import Research from "./pages/Research/Research";
 import Publication from "./pages/Publication/Publication";
 import Lecture from "./pages/Lecture/Lecture";
@@ -18,11 +18,10 @@ import Gallery from "./pages/Community/Gallery";
 import Notice from "./pages/Community/Notice";
 import Contact from "./pages/Contact/Contact";
 import Layout from "./components/Layout/Layout";
-import Detail from "./components/Post/Card/Detail";
-import Write from "./pages/Research/Write";
 import IntroductionWrite from "./pages/Introduction/Introduction/IntroductionWrite";
 import Viewer from "./components/Editor/Viewer";
 import ProfessorWrite from "./pages/Introduction/Professor/ProfessorWrite";
+import Members from "./pages/Members";
 
 const App = (props) => {
 
@@ -33,41 +32,42 @@ const App = (props) => {
                     <Route path={'/user/regist/input'} component={Register}/>
                     <Route path={'/login'} component={Login}/>
 
-                    {/* 구조화 리팩토링 예정 */}
-                    {/* url 오타 리다이랙트 리팩토링 예정 */}
+                    {/* 리다이렉트 처리 : 오타, url에 params 추가 */}
 
                     <Route exact path={'/'} component={Viewer}/>
 
-                    {/* introduction */}
+                    {/* introduction & research */}
+                    {/* 컨텐츠 구성 질의, exact path 수정 */}
                     <Route exact path={'/introduction'} component={Introduction}/>
                     <Route exact path={'/introduction/write'} component={IntroductionWrite}/>
-                    <Route exact path={'/introduction/professor'} component={Professor}/>
-                    <Route exact path={'/introduction/professor/write'} component={ProfessorWrite}/>
-
-                    <Route exact path={'/introduction/people/currentMember'} component={CurrentMember}/>
-                    <Route exact path={'/introduction/people/currentMember/:memberId'} component={CurrentMember}/>
-
-                    <Route exact path={'/introduction/people/alumni'} component={Alumni}/>
-                    <Route exact path={'/introduction/people/alumni/:alumniId'} component={Alumni}/>
-
-
-                    {/* research */}
                     <Route exact path={'/research'} component={Research}/>
 
+                    {/* professor */}
+                    {/* 한 분인지, 사이드바에 여러명 추가되는 지 */}
+                    <Route exact path={'/professor/write'} component={ProfessorWrite}/>
+                    <Route path={'/professor'} component={Professor}/>
+
+                    {/* members */}
+                    <Route path={'/members/current'} component={CurrentMember}/>
+                    <Route path={'/members/current/:id'} component={CurrentMember}/>
+                    <Route path={'/members/alumni'} component={Alumni}/>
+                    <Route path={'/members/alumni/:id'} component={Alumni}/>
+                    <Route path={'/members'} component={Members}/>
+
                     {/* publications */}
-                    <Route exact path={'/publication'} component={Publication}/>
+                    <Route path={'/publication'} component={Publication}/>
 
                     {/* lecture */}
-                    <Route exact path={'/lecture'} component={Lecture}/>
+                    <Route path={'/lecture'} component={Lecture}/>
                     <Route exact path={'/lecture/:name'} component={Lecture}/>
 
                     {/* community */}
                     <Route exact path={'/community/gallery'} component={Gallery}/>
                     <Route exact path={'/community/notice'} component={Notice}/>
+                    {/*<Route exact path={'/community/board'} component={Board}/>*/}
 
                     {/* contact */}
                     <Route exact path={'/contact'} component={Contact}/>
-
 
                     <Route path={'/error/404'} component={Page404}/>
                     <Redirect to={'/error/404'}/>
