2071d948bb4c33d94859c8bb71c0bad26dd7cd24
BY <devblossom77@gmail.com>
2020-10-27 20:59:44 +0900

    [login] auth hoc 추가

--- src/App.js
+++ src/App.js
@@ -22,14 +22,24 @@ import IntroductionWrite from "./pages/Introduction/Introduction/IntroductionWri
 import Viewer from "./components/Editor/Viewer";
 import ProfessorWrite from "./pages/Introduction/Professor/ProfessorWrite";
 import Members from "./pages/Members";
+import {appCreators} from "./redux/actionCreators";
+import Auth from "./hoc/Auth";
 
 const App = (props) => {
 
+    const {toastMessage} = useSelector(state => state.app);
+    useEffect(() => {
+        appCreators.setToastMessage('토스트 메세지입니다.(4초)');
+    }, [])
+
     return (
         <Wrapper>
+            {
+                toastMessage && <ToastMessage message={toastMessage}/>
+            }
             <Layout>
                 <Switch>
-                    <Route path={'/user/regist/input'} component={Register}/>
+                    <Route path={'/register'} component={Register}/>
                     <Route path={'/login'} component={Login}/>
 
                     {/* 리다이렉트 처리 : 오타, url에 params 추가 */}
@@ -38,38 +48,38 @@ const App = (props) => {
 
                     {/* introduction & research */}
                     {/* 컨텐츠 구성 질의, exact path 수정 */}
-                    <Route exact path={'/introduction'} component={Introduction}/>
-                    <Route exact path={'/introduction/write'} component={IntroductionWrite}/>
-                    <Route exact path={'/research'} component={Research}/>
+                    <Route exact path={'/introduction'} component={Auth(Introduction, null)}/>
+                    <Route exact path={'/introduction/write'} component={Auth(IntroductionWrite, true)}/>
+                    <Route exact path={'/research'} component={Auth(Research, null)}/>
 
                     {/* professor */}
                     {/* 한 분인지, 사이드바에 여러명 추가되는 지 */}
-                    <Route exact path={'/professor/write'} component={ProfessorWrite}/>
-                    <Route path={'/professor'} component={Professor}/>
+                    <Route exact path={'/professor/write'} component={Auth(ProfessorWrite, true)}/>
+                    <Route path={'/professor'} component={Auth(Professor)}/>
 
                     {/* members */}
-                    <Route path={'/members/current'} component={CurrentMember}/>
-                    <Route path={'/members/current/:id'} component={CurrentMember}/>
-                    <Route path={'/members/alumni'} component={Alumni}/>
-                    <Route path={'/members/alumni/:id'} component={Alumni}/>
-                    <Route path={'/members'} component={Members}/>
+                    <Route path={'/members/current'} component={Auth(CurrentMember, null)}/>
+                    <Route path={'/members/current/:id'} component={Auth(CurrentMember, null)}/>
+                    <Route path={'/members/alumni'} component={Auth(Alumni, null)}/>
+                    <Route path={'/members/alumni/:id'} component={Auth(Alumni, null)}/>
+                    <Route path={'/members'} component={Auth(Members, null)}/>
 
                     {/* publications */}
-                    <Route path={'/publication'} component={Publication}/>
+                    <Route path={'/publication'} component={Auth(Publication, null)}/>
 
                     {/* lecture */}
-                    <Route path={'/lecture'} component={Lecture}/>
-                    <Route exact path={'/lecture/:name'} component={Lecture}/>
+                    <Route path={'/lecture'} component={Auth(Lecture, null)}/>
+                    <Route exact path={'/lecture/:name'} component={Auth(Lecture, null)}/>
 
                     {/* community */}
-                    <Route exact path={'/community/gallery'} component={Gallery}/>
-                    <Route exact path={'/community/notice'} component={Notice}/>
-                    {/*<Route exact path={'/community/board'} component={Board}/>*/}
+                    <Route exact path={'/community/gallery'} component={Auth(Gallery, null)}/>
+                    <Route exact path={'/community/notice'} component={Auth(Notice, null)}/>
+                    {/*<Route exact path={'/community/board'} component={Auth(Board, null}/>*/}
 
                     {/* contact */}
-                    <Route exact path={'/contact'} component={Contact}/>
+                    <Route exact path={'/contact'} component={Auth(Contact, null)}/>
 
-                    <Route path={'/error/404'} component={Page404}/>
+                    <Route path={'/error/404'} component={Auth(Page404, null)}/>
                     <Redirect to={'/error/404'}/>
                 </Switch>
             </Layout>
